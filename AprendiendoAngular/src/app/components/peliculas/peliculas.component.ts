63246b02b728ae9f9094749b91a53a860933a931
mario robayo <marioferyhwh@gmail.com>
2020-05-12 13:46:30 -0500

    modelo

--- AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, DoCheck, OnDestroy } from "@angular/core";
+import Pelicula from "src/app/models/pelicula";
 
 @Component({
   selector: "app-peliculas",
@@ -7,32 +8,30 @@ import { Component, OnInit, DoCheck, OnDestroy } from "@angular/core";
 })
 export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
   titulo: string;
-  peliculas: Array<any>;
+  peliculas: Pelicula[];
   constructor() {
     this.titulo = "peliculas";
     this.peliculas = [
-      {
-        year: 2019,
-        title: "spider",
-        img: "https://i.ytimg.com/vi/PUSp8ox1QYo/maxresdefault.jpg",
-      },
-      {
-        year: 2018,
-        title: "vengadores",
-        img:
-          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vengadores-marvel-netflix-1554116074.jpg?crop=1.00xw:0.445xh;0,0.134xh&resize=480:*",
-      },
-      {
-        year: 2015,
-        title: "bantamn vs superman barata",
-        img: "https://i.ytimg.com/vi/PbSw8cP_6W4/maxresdefault.jpg",
-      },
-      {
-        year: 2011,
-        title: "otra",
-        img:
-          "https://es.web.img2.acsta.net/medias/nmedia/18/89/61/50/20058082.jpg",
-      },
+      new Pelicula(
+        "spider",
+        2019,
+        "https://i.ytimg.com/vi/PUSp8ox1QYo/maxresdefault.jpg"
+      ),
+      new Pelicula(
+        "vengadores",
+        2018,
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vengadores-marvel-netflix-1554116074.jpg?crop=1.00xw:0.445xh;0,0.134xh&resize=480:*"
+      ),
+      new Pelicula(
+        "bantamn vs superman barata",
+        2015,
+        "https://i.ytimg.com/vi/PbSw8cP_6W4/maxresdefault.jpg"
+      ),
+      new Pelicula(
+        "otra",
+        2011,
+        "https://es.web.img2.acsta.net/medias/nmedia/18/89/61/50/20058082.jpg"
+      ),
     ];
   }
 
---
+++ AprendiendoAngular/src/app/models/pelicula.ts
@@ -0,0 +1,13 @@
+class Pelicula {
+  // title: string;
+  // img: string;
+  // year: number;
+  // constructor(title, year, img) {
+  //   this.title = title;
+  //   this.year = year;
+  //   this.img = img;
+  // }
+  constructor(public title: string, public year: number, public img: string) {}
+}
+
+export default Pelicula;
