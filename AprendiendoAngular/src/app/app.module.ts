a0979b4e85c1e184d2efaa9e21491cb92916d0a6
mario robayo <marioferyhwh@gmail.com>
2020-05-12 11:02:46 -0500

    se genera los componentes del maquetado de index

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { MiComponentComponent } from "./components/mi-component/mi-component.component";
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiComponentComponent,
-    PeliculasComponent
+    PeliculasComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [BrowserModule],
   providers: [],
