ed083d544a9c61db287071fc168c041d25e81ce3
mario robayo <marioferyhwh@gmail.com>
2020-05-12 11:14:33 -0500

    se le informa app la nueva direcion d els componentes

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { MiComponentComponent } from "./components/mi-component/mi-component.component";
-import { PeliculasComponent } from './components/peliculas/peliculas.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SliderComponent } from './components/slider/slider.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { BlogComponent } from './blog/blog.component';
-import { FormularioComponent } from './formulario/formulario.component';
-import { PaginaComponent } from './pagina/pagina.component';
+import { PeliculasComponent } from "./components/peliculas/peliculas.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SliderComponent } from "./components/slider/slider.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from "./components/home/home.component";
+import { BlogComponent } from "./components/blog/blog.component";
+import { FormularioComponent } from "./components/formulario/formulario.component";
+import { PaginaComponent } from "./components/pagina/pagina.component";
 
 @NgModule({
   declarations: [
@@ -25,7 +25,7 @@ import { PaginaComponent } from './pagina/pagina.component';
     HomeComponent,
     BlogComponent,
     FormularioComponent,
-    PaginaComponent
+    PaginaComponent,
   ],
   imports: [BrowserModule],
   providers: [],
