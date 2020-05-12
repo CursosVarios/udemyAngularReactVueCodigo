6e85b1010d07bbcc172d3e70d08d988a5ff621d3
mario robayo <marioferyhwh@gmail.com>
2020-05-12 14:11:51 -0500

    envia propiedades a slider

--- AprendiendoAngular/src/app/components/slider/slider.component.ts
+++ AprendiendoAngular/src/app/components/slider/slider.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-slider',
-  templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  selector: "app-slider",
+  templateUrl: "./slider.component.html",
+  styleUrls: ["./slider.component.scss"],
 })
 export class SliderComponent implements OnInit {
+  //se resibe la propiedad nombre
+  @Input() nombre: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
