902dbad795abc89c6afd6809645640eaa20421d6
mario robayo <marioferyhwh@gmail.com>
2020-05-13 16:21:44 -0500

    sidebar ahora responde las busqeudas dinamicamente

--- AprendiendoAngular/src/app/components/sidebar/sidebar.component.ts
+++ AprendiendoAngular/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
 })
 export class SidebarComponent implements OnInit {
+  public search: string;
+  private url: string;
+  constructor(private _router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goSearch() {
+    // this._router.navigate(["/buscar/" + this.search]);
+
+    this._router.navigate(["/buscar", this.search]);
   }
-
 }
