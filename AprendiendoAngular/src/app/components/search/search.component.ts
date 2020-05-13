cfe089ff12f96aa43ddcf913e2739d0de631fe46
mario robayo <marioferyhwh@gmail.com>
2020-05-13 15:40:28 -0500

    se  agrea mensaje cuando no haya articulos se configuranr funciones de busqueda

--- AprendiendoAngular/src/app/components/search/search.component.ts
+++ AprendiendoAngular/src/app/components/search/search.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router, Params } from "@angular/router";
+import { Article } from "src/app/models/article";
+import { ArticuleService } from "src/app/services/article.service";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.scss"],
+  providers: [ArticuleService],
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  public articles: Article[];
+  public search: string;
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _articleservice: ArticuleService
+  ) {}
 
   ngOnInit(): void {
+    this._route.params.subscribe((params: Params) => {
+      const search = params["search"];
+      this.search = search;
+      this._articleservice.search(search).subscribe(
+        (res) => {
+          console.log(res);
+          console.log("entrada");
+          if (res.art) {
+            this.articles = res.art;
+          } /* else {
+            alert("no se encontro nada");
+            //this._router.navigate(["/home"]);
+          } */
+        },
+        (err) => {
+          console.log(err);
+          this.articles = [];
+          // alert("no se encontro nada");
+        }
+      );
+    });
   }
-
 }
