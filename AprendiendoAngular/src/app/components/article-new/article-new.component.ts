a5d4c4d1f300b3a602b3cbef7f85c15aaadbe290
mario robayo <marioferyhwh@gmail.com>
2020-05-13 18:39:11 -0500

    se estructura el formulario de creacio

--- AprendiendoAngular/src/app/components/article-new/article-new.component.ts
+++ AprendiendoAngular/src/app/components/article-new/article-new.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Article } from "src/app/models/article";
 
 @Component({
-  selector: 'app-article-new',
-  templateUrl: './article-new.component.html',
-  styleUrls: ['./article-new.component.scss']
+  selector: "app-article-new",
+  templateUrl: "./article-new.component.html",
+  styleUrls: ["./article-new.component.scss"],
 })
 export class ArticleNewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public article: Article;
+  constructor() {
+    this.article = new Article("", "", "", null, null);
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.article);
+  }
 }
