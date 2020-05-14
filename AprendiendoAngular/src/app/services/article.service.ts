d8e0c8aee38e502f5f46f61bc68919fa381cd555
mario robayo <marioferyhwh@gmail.com>
2020-05-13 21:59:42 -0500

    se agregam servicios para editar

--- AprendiendoAngular/src/app/services/article.service.ts
+++ AprendiendoAngular/src/app/services/article.service.ts
@@ -32,4 +32,15 @@ export class ArticleService {
 
     return this._http.post(this.url + "/save", params, { headers });
   }
+  update(article): Observable<any> {
+    const params = JSON.stringify(article);
+    const headers = new HttpHeaders().set("Content-Type", "application/json");
+    return this._http.put(this.url + "article/" + article._id, params, {
+      headers,
+    });
+  }
+  delete(id): Observable<any> {
+    const headers = new HttpHeaders().set("Content-Type", "application/json");
+    return this._http.delete(this.url + "article/" + id, { headers });
+  }
 }
