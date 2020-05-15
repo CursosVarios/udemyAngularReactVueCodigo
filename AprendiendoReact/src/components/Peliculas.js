375a00de8dba930b0c1b9ef386f12dd4274cd9dc
mario robayo <marioferyhwh@gmail.com>
2020-05-14 20:55:29 -0500

    siclo de vida

--- AprendiendoReact/src/components/Peliculas.js
+++ AprendiendoReact/src/components/Peliculas.js
@@ -2,65 +2,76 @@ import React, { Component } from "react";
 import Pelicula from "./Pelicula";
 
 class Peliculas extends Component {
-  state = {
-    peliculas: [
-      {
-        title: "batman vs superman",
-        image:
-          "https://images-na.ssl-images-amazon.com/images/I/911RR0tzL2L._AC_SL1500_.jpg",
-        date: 2018,
-      },
-      {
-        title: "Spiderman",
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQx3-8kLIktys2qe0Do9Ca05-Mx-yE556SFwZXHsqOi33vqvdT5&usqp=CAU",
-        date: 2012,
-      },
-      {
-        title: "looper",
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
-        date: 2018,
-      },
-      {
-        title: "looper",
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
-        date: 2018,
-      },
-      {
-        title: "looper",
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
-        date: 2018,
-      },
-      {
-        title: "looper",
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
-        date: 2018,
-      },
-      {
-        title: "looper",
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
-        date: 2018,
-      },
-      {
-        title: "looper",
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
-        date: 2018,
-      },
-      {
-        title: "looper",
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
-        date: 2018,
-      },
-    ],
-    favorita: null,
-  };
+  state = {};
+
+  componentWillMount() {
+    this.setState({
+      peliculas: [
+        {
+          title: "batman vs superman",
+          image:
+            "https://images-na.ssl-images-amazon.com/images/I/911RR0tzL2L._AC_SL1500_.jpg",
+          date: 2018,
+        },
+        {
+          title: "Spiderman",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQx3-8kLIktys2qe0Do9Ca05-Mx-yE556SFwZXHsqOi33vqvdT5&usqp=CAU",
+          date: 2012,
+        },
+        {
+          title: "looper",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
+          date: 2018,
+        },
+        {
+          title: "looper",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
+          date: 2018,
+        },
+        {
+          title: "looper",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
+          date: 2018,
+        },
+        {
+          title: "looper",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
+          date: 2018,
+        },
+        {
+          title: "looper",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
+          date: 2018,
+        },
+        {
+          title: "looper",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
+          date: 2018,
+        },
+        {
+          title: "looper",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyAMEw4Pg_MTY4e_mCK5xIzw32lQ6Tf4SPnCGE8x4bTytCbB4P&usqp=CAU",
+          date: 2018,
+        },
+      ],
+      favorita: null,
+    });
+  }
+  componentDidMount() {
+    console.log("el componente se monto");
+  }
+
+  componentWillUnmount() {
+    console.log("componete se  va a eliminar");
+  }
   CambiarTitulo = () => {
     let { peliculas } = this.state;
 
