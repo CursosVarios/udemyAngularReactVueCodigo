17430a2c3473f02239bcd036b7028849d0b24efe
mario robayo <marioferyhwh@gmail.com>
2020-05-17 11:34:39 -0500

    se instala axios se realiza puebla de axions en blog se crea el componente articulos donde se traeran lo articulos desde la api

---
+++ AprendiendoReact/src/components/Articles.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Articles extends Component {
+  render() {
+    return (
+      <div className="articles">
+        <h2>hola</h2>
+      </div>
+    );
+  }
+}
+
+export default Articles;
--- AprendiendoReact/src/components/Blog.js
+++ AprendiendoReact/src/components/Blog.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 import Slider from "./Slider";
 import Sidebar from "./Sidebar";
+import Axios from "axios";
+import Articles from "./Articles";
 
 class Blog extends Component {
+  state = {
+    articles: {},
+  };
+
   render() {
+    Axios.get("http://localhost:8080/api/articles")
+      .then((res) => {
+        // this.setState({
+        //   articles: res.data.article,
+        // });
+        console.log(res.data.article);
+      })
+      .catch((err) => console.log("ERROR", err));
     return (
       <React.Fragment>
         <Slider title=" Pagina Blog" size="slider-small" />
@@ -11,6 +25,12 @@ class Blog extends Component {
           <div className="content">
             {/* <h1 className="subheader">Ultimos articulos</h1>
              */}
+
+            {this.state.articles.length > 0 &&
+              this.state.articles.map((article) => {
+                return <h1 key={article._id}> {article._id}</h1>;
+              })}
+            <Articles></Articles>
           </div>
           <Sidebar blog="true" />
         </div>
