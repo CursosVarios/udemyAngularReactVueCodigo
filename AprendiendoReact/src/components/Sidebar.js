30ceabe62580f1ef561981d41a4b1a9c4c83672b
mario robayo <marioferyhwh@gmail.com>
2020-05-17 17:53:41 -0500

    se configura direcionamiento para buscar

--- AprendiendoReact/src/components/Sidebar.js
+++ AprendiendoReact/src/components/Sidebar.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 
 class Sidebar extends Component {
+  seatchRef = React.createRef();
+  state = {
+    search: "",
+    redirec: "false",
+  };
+  redirecSearch = (e) => {
+    e.preventDefault();
+    console.log(this.seatchRef);
+    this.setState({
+      search: this.seatchRef.current.value,
+      redirec: "true",
+    });
+  };
   render() {
+    if (this.state.redirec === "true") {
+      return <Redirect to={"/blog/busqueda/" + this.state.search}></Redirect>;
+    }
     return (
       <aside id="sidebar">
         {this.props.blog === "true" && (
           <div className="sidebat-item" id="nav-blog">
             <h3>Puedes hacer esto </h3>
-            <NavLink to="/blog/nuevo" className="btn btn-success">
+            <Link to="/blog/nuevo" className="btn btn-success">
               crear articulo
-            </NavLink>
+            </Link>
           </div>
         )}
         <div className="sidebat-item" id="search">
           <h3>Puedes hacer esto </h3>
           <p>encuentra el articulo que buscas</p>
-          <form action="#">
-            <input type="text" name="search" />
+          <form action="#" onSubmit={this.redirecSearch}>
+            <input type="text" name="search" ref={this.seatchRef} />
             <input type="submit" name="submit" value="buscar" className="btn" />
           </form>
         </div>
