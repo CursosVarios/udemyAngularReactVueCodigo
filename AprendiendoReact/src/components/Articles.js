8390617e457d4372e312dc1fe5da1d690d6bc9e2
mario robayo <marioferyhwh@gmail.com>
2020-05-17 13:54:41 -0500

    se mejora el codigo de traer articulos

--- AprendiendoReact/src/components/Articles.js
+++ AprendiendoReact/src/components/Articles.js
@@ -11,25 +11,13 @@ class Articles extends Component {
   url = Global.url;
   componentWillMount() {
     if (this.props.home === "true") {
-      this.getArticlesLast();
+      this.getArticles(1);
     } else {
       this.getArticles();
     }
   }
-  getArticles = () => {
-    Axios.get(this.url + "articles")
-      .then((res) => {
-        this.setState({
-          articles: res.data.article,
-          status: "success",
-        });
-        console.log(res.data.article);
-      })
-      .catch((err) => console.log("ERROR", err));
-  };
-
-  getArticlesLast = () => {
-    Axios.get(this.url + "articles/3")
+  getArticles = (last) => {
+    Axios.get(this.url + "articles" + (last ? "/1" : ""))
       .then((res) => {
         this.setState({
           articles: res.data.article,
