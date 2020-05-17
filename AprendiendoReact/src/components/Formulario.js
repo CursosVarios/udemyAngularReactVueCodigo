f6a160c90a3d590d114e0885260f21c214f2bcfd
mario robayo <marioferyhwh@gmail.com>
2020-05-17 10:17:24 -0500

    se prueba formulario reactivo

--- AprendiendoReact/src/components/Formulario.js
+++ AprendiendoReact/src/components/Formulario.js
@@ -8,15 +8,27 @@ class Formulario extends Component {
   GHRef = React.createRef();
   GMRef = React.createRef();
   GORef = React.createRef();
+
+  state = {
+    user: {},
+  };
   recibirFormularo = (e) => {
     e.preventDefault();
-    alert("enviado");
-    console.log(this.nombreRef.current.value);
-    console.log(this.apellidoRef.current.value);
-    console.log(this.bioRef.current.value);
-    console.log(this.GHRef.current.value);
-    console.log(this.GMRef.current.value);
-    console.log(this.GORef.current.value);
+    const user = {
+      nombre: this.nombreRef.current.value,
+      apellido: this.apellidoRef.current.value,
+      bio: this.bioRef.current.value,
+      Genero: this.GHRef.current.checked
+        ? this.GHRef.current.value
+        : this.GMRef.current.checked
+        ? this.GMRef.current.value
+        : this.GORef.current.value,
+    };
+    //alert("enviado");
+    console.log(user);
+    this.setState({
+      user: user,
+    });
   };
   render() {
     return (
@@ -25,10 +37,21 @@ class Formulario extends Component {
           <div className="content">
             {/* <h1 className="subheader">Ultimos articulos</h1> */}
             <h1 className="subheader">Formulario </h1>
+
+            {this.state.user.nombre && (
+              <ul>
+                <li>{this.state.user.nombre}</li>
+                <li>{this.state.user.apellido}</li>
+                <li>{this.state.user.bio}</li>
+                <li>{this.state.user.genero}</li>
+              </ul>
+            )}
+
             <form
               action=""
               className="mid-form"
               onSubmit={this.recibirFormularo}
+              onChange={this.recibirFormularo}
             >
               <div className="form-group">
                 <label htmlFor="nombre">Nombre</label>
