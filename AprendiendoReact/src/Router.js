ffc4be2af2cca390b34ee4e2530ae179f9f3f0b3
mario robayo <marioferyhwh@gmail.com>
2020-05-16 19:35:46 -0500

    pagina 404

--- AprendiendoReact/src/Router.js
+++ AprendiendoReact/src/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SeccionPruebas from "./components/SeccionPruebas";
 import MiComponente from "./components/MiComponente";
 import Peliculas from "./components/Peliculas";
+import Error from "./components/Error";
 class Router extends Component {
   render() {
     return (
@@ -13,6 +14,7 @@ class Router extends Component {
           <Route exact path="/" component={Peliculas} />
           <Route exact path="/ruta-prueba" component={SeccionPruebas} />
           <Route exact path="/mi-componente" component={MiComponente} />
+          <Route component={Error} />
         </Switch>
       </BrowserRouter>
     );
---
+++ AprendiendoReact/src/components/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Error = () => {
+  return (
+    <sectio id="content">
+      <h2 className="subheader">Pagina no encontrada</h2>
+    </sectio>
+  );
+};
+
+export default Error;
