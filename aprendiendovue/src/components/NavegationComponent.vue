07928bf54e895d385c43bce0b8379f4570a839c4
mario robayo <marioferyhwh@gmail.com>
2020-05-18 14:30:11 -0500

    se indica donde se encuentra en la varra de navegacion

--- aprendiendovue/src/components/NavegationComponent.vue
+++ aprendiendovue/src/components/NavegationComponent.vue
@@ -2,21 +2,27 @@
   <nav id="menu">
     <ul>
       <li>
-        <router-link to="/home">Inicio</router-link>
+        <router-link to="/home" active-class="active">Inicio</router-link>
       </li>
       <li>
-        <router-link to="/blog">Blog</router-link>
+        <router-link to="/blog" active-class="active">Blog</router-link>
       </li>
       <li>
-        <router-link to="/formulario">Formulario</router-link>
+        <router-link to="/formulario" active-class="active"
+          >Formulario</router-link
+        >
       </li>
       <li>
-        <router-link :to="{ name: 'page', params: { id: 5 } }"
+        <router-link
+          :to="{ name: 'page', params: { id: 5 } }"
+          active-class="active"
           >pagina1</router-link
         >
       </li>
       <li>
-        <router-link to="/mi-componente">pagina2</router-link>
+        <router-link to="/mi-componente" active-class="active"
+          >pagina2</router-link
+        >
       </li>
     </ul>
   </nav>
