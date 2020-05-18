e218983ef3129798720c8d654be6554b92f37dd8
mario robayo <marioferyhwh@gmail.com>
2020-05-18 15:44:19 -0500

    v-for

--- aprendiendovue/src/components/MiComponente.vue
+++ aprendiendovue/src/components/MiComponente.vue
@@ -3,13 +3,26 @@
     <h1>titulo de mi componente</h1>
     <h2>{{ subtitulo }}</h2>
 
-    <h3>reactividad</h3>
+    <h2>reactividad</h2>
     <input type="text" v-model="datoDeInput" />
     <pre>
       {{ $data }}
-      </pre
-    >
+      </pre>
     {{ datoDeInput }}
+    <h2>directivas</h2>
+    <input type="number" v-model="edad" />
+    la edad es
+    <strong>{{edad}}</strong>
+    <br />
+    <p class="alert-danger-provisional" v-if="edad && edad >= 60">tercera edad</p>
+    <p class="alert-danger-provisional" v-else-if="edad && edad >= 18">Mayor de edad</p>
+    <!-- <p class="alert-danger-provisional" v-if="edad < 18">Menor de edad</p> -->
+    <p class="alert-danger-provisional" v-else>Menor de edad</p>
+    <hr />
+    <h2>Directiva iterativa</h2>
+    <ol>
+      <li v-for="pelicula in peliculas" :key="pelicula">{{pelicula}}</li>
+    </ol>
   </div>
 </template>
 
@@ -20,8 +33,10 @@ export default {
     return {
       subtitulo: "subtitulo componente",
       datoDeInput: "",
+      edad: 0,
+      peliculas: ["bataman", "spiderman", "el señor de los anillos"]
     };
-  },
+  }
 };
 </script>
 
