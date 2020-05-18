d6417873d97e12f22c8f21f25da8ac918a4e63f7
mario robayo <marioferyhwh@gmail.com>
2020-05-18 14:19:34 -0500

    evento de Boton

--- aprendiendovue/src/components/PaginaComponent.vue
+++ aprendiendovue/src/components/PaginaComponent.vue
@@ -2,6 +2,8 @@
   <section id="content">
     <h2 class="subheader">Pagina pruebas</h2>
     <h3>{{ id_desde_url }}</h3>
+    {{ nombre }}
+    <button v-on:click="actualizarNombre()">Actualizar Nombre</button>
   </section>
 </template>
 
@@ -11,10 +13,19 @@ export default {
   mounted() {
     this.id_desde_url = this.$route.params.id;
   },
+  created() {},
+  updated() {},
+  destroyed() {},
   data() {
     return {
       id_desde_url: null,
+      nombre: "nombre 1",
     };
+  },
+  methods: {
+    actualizarNombre() {
+      this.nombre = "nuevo nombre asignado";
+    },
   },
 };
 </script>
