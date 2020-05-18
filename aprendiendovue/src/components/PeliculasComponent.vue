ccfe8ff11897c12974bb313aacad9d8028bb4f93
mario robayo <marioferyhwh@gmail.com>
2020-05-18 16:21:26 -0500

    bindin y se aregla imagenes

--- aprendiendovue/src/components/PeliculasComponent.vue
+++ aprendiendovue/src/components/PeliculasComponent.vue
@@ -9,7 +9,14 @@
         </div>
         <h2>{{pelicula.title}}</h2>
 
-        <span class="date">{{pelicula.year}}</span>
+        <span
+          class="date"
+          v-bind:class="{
+          yeargreen:pelicula.year > 2018,
+          yearred:pelicula.year <= 2018
+        }
+        "
+        >{{pelicula.year}}</span>
         <a href="#">leer mas</a>
         <div class="clearfix"></div>
       </article>
