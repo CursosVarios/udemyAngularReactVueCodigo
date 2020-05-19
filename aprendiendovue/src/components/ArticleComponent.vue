566e1cc092d7fcd11a4c3576384b732287bf1978
mario robayo <marioferyhwh@gmail.com>
2020-05-19 10:59:20 -0500

    se agrega carganso

--- aprendiendovue/src/components/ArticleComponent.vue
+++ aprendiendovue/src/components/ArticleComponent.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="articles">
+    <div v-if="!articles">...Cargando</div>
+
     <article class="article-item" v-for="article in articles" :key="article._id">
       <div class="image-wrap">
         <img :src="url+'get-image/'+article.image" alt="imagen estrellas" v-if="article.image" />
         <img src="../assets/image/no-image.jpg" alt="imagen estrellas" v-if="!article.image" />
       </div>
       <h2>{{article.title}}</h2>
-
       <span class="date">{{article.date}}</span>
       <router-link to="'/blog/article'+article._id">leer mas</router-link>
       <div class="clearfix"></div>
     </article>
+
+    <div v-if="articles && articles.length===0">No hay articulos para mostart</div>
   </div>
 </template>
 
